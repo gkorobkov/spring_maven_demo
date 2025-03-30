b86f5040bb821246555e2e332e034b41ebeb3b75
George K. <gkorobkov@gmail.com>
2025-03-31 00:10:33 +0300

    remove unused imports

--- src/main/java/com/example/spring_maven_demo/repository/AccountRepository.java
+++ src/main/java/com/example/spring_maven_demo/repository/AccountRepository.java
@@ -7,8 +7,6 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,9 +14,6 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    // @NativeQuery(value = "")
-    // Account getAccountWithSkipLock() ;
 
     // Поиск по клиенту
     List<Account> findByClient(String client);
