eeea84bb1050d4221d69d95729622afbafc5b6c6
George K. <gkorobkov@gmail.com>
2025-04-05 20:54:22 +0300

    small update

--- src/main/java/com/example/spring_maven_demo/SpringMavenDemoApplication.java
+++ src/main/java/com/example/spring_maven_demo/SpringMavenDemoApplication.java
@@ -1,23 +1,30 @@
 package com.example.spring_maven_demo;
 
 import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMavenDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMavenDemoApplication.class, args);
-		Map<String, String> env_var = System.getenv();
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringMavenDemoApplication.class, args);
+        Map<String, String> env_var = System.getenv();
 
         // Loop through all environment variables
+
+        System.out.println();
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!! System.getenv() ");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println();
+
         for (String envName : env_var.keySet()) {
             // Print environment variable name and value to console
             System.out.format("%s=%s", envName, env_var.get(envName));
             System.out.println();
         }
-	}
 
+    }
 }
