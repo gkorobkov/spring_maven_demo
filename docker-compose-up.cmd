c633006342beca7dc199406083c6b2a1c6a2d471
George K. <gkorobkov@gmail.com>
2025-04-07 22:02:00 +0300

    Add docker compose support

---
+++ docker-compose-up.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+rem  docker run --name app-spring-maven-demo   -p 8080:8080   --env-file=.env   img-spring-maven-demo
+rem docker-compose --file docker-compose.yaml up --detach
+
+docker-compose up --detach
+
