0ea43f5336e6df639442956fef7668d825e76a43
George K. <gkorobkov@gmail.com>
2025-04-12 23:04:39 +0300

    Update docker commands

--- docker-commands.cmd
+++ docker-commands.cmd
@@ -1,1 +1,3 @@
  docker build -t img-spring-maven-demo .
+ docker run --name app-spring-maven-demo   -p 8080:8080   --env-file=.env   img-spring-maven-demo
+
--- docker-run.cmd
+++
@@ -1,2 +0,0 @@
- docker run --name app-spring-maven-demo   -p 8080:8080   --env-file=.env   img-spring-maven-demo
-
