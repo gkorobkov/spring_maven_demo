6071fff6867a7949ed3fe136dc694c3c608486c1
George K. <gkorobkov@gmail.com>
2025-04-16 22:21:32 +0300

    Update cmd; add env to docker compose

--- docker-commands.cmd
+++ docker-commands.cmd
@@ -1,4 +1,4 @@
- rem docker build -t img-spring-maven-demo .
- docker rm app-dev-spring-maven-demo
- docker run --name app-dev-spring-maven-demo   -p 8081:8080   --env-file=dev.env   img-spring-maven-demo
+rem docker build -t img-spring-maven-demo .
+rem docker rm app-dev-spring-maven-demo
+docker run --name app-dev-spring-maven-demo   -p 8081:8080   --env-file=dev.env   img-spring-maven-demo
 
